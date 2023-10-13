0c1db6dc7426d439efa2bce837d4f0f91fbc4dac
Lee Kyuri <99018663+kyuri0927@users.noreply.github.com>
2023-10-13 20:33:31 +0900

    Add files via upload
    last modified

---
+++ grad_project/js/share.js
@@ -0,0 +1,40 @@
+const url = 'https://gradtestt3.netlify.app/';
+
+function setShare() {
+    // 음식결과
+    var resultImg = document.querySelector('#resultImg');
+    var resultAlt = resultImg.firstElementChild.alt;
+    // 카카오 공유했을 때 나올 타이틀
+    const shareTitle = '수슐랭 추천 결과';
+    // 공유했을 때 나올 음식 이름
+    const shareDes = infoList[resultAlt].name;
+    // 각 음식 사진주소
+    const shareImage = url + 'img/image-' + resultAlt + '.png';
+    // 공유 링크
+    const shareURL = url + 'page/result-' + resultAlt + '.html';
+
+
+    Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+            title: shareTitle,
+            description: shareDes,
+            imageUrl: shareImage,
+            link: {
+                mobileWebUrl: shareURL,
+                webUrl: shareURL
+            },
+        },
+
+        buttons: [
+            {
+                title: '결과확인하기',
+                link: {
+                    mobileWebUrl: shareURL,
+                    webUrl: shareURL,
+                },
+            },
+        ]
+    });
+
+}
